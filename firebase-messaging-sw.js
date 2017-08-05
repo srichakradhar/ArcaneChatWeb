88cdcfa555bc8e9d5c0c61aeec09888e5b6b6178
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2017-08-06 00:30:53 +0530

    Initial commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chuck on 05/08/17.
+ */
+importScripts('/__/firebase/3.8.0/firebase-app.js');
+importScripts('/__/firebase/3.8.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+firebase.messaging();
